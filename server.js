250847386e8c0f8447cfc37059f7d22e0fda69be
linhbui98 <linhbuiquang2321998@gmail.com>
2020-07-22 17:29:23 +0700

    test

--- server.js
+++ server.js
@@ -11,14 +11,10 @@ const indexRoute = require('./routes/index');
 const http1app = Express();
 http1app.use(Express.static('assets'));
 
-// HTTP2
-const http2app = Express();
-
 http1app.set('views', path.join(__dirname, 'views'));
 http1app.set('view engine', 'pug');
 http1app.get('/', indexRoute)
 
 Https.createServer(certificate, http1app).listen(PORT, () => {
-    Spdy.createServer(certificate, http2app).listen(process.env.PORT2 || 3000)
     console.log("App running")
 });
