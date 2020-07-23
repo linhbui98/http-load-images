cfffede3242425aa957dc9d9608b8620df47277a
linhbui98 <linhbuiquang2321998@gmail.com>
2020-07-23 13:37:15 +0700

    add load time

--- views/script.js
+++ views/script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    let loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+    let loadTime = Date.now() - window.performance.timing.navigationStart;
     loadTime = (loadTime / 1000).toFixed(2);
     document.getElementById('timeLoad').textContent = loadTime;
 }
