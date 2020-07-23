50a5966a43fc642169099eb0faf8de8d114970b1
linhbui98 <linhbuiquang2321998@gmail.com>
2020-07-23 08:41:50 +0700

    remove cache

--- server.js
+++ server.js
@@ -9,12 +9,18 @@ const indexRoute = require('./routes/index');
 
 // HTTP1
 const http1app = Express();
+http1app.use(function(req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next()
+});
 http1app.use(Express.static('assets'));
 
 http1app.set('views', path.join(__dirname, 'views'));
 http1app.set('view engine', 'pug');
 http1app.get('/', indexRoute)
 
-Https.createServer(http1app).listen(PORT, () => {
+Https.createServer(certificate, http1app).listen(PORT, () => {
     console.log("App running")
 });
